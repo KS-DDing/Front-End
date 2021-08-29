57788207e6fac8c26c7aa2c708ba9983f3d6cf92
DDing77 <smg76262585@gmail.com>
2021-08-29 21:08:00 +0900

    Update LandingPage.js

--- src/Page/LandingPage.js
+++ src/Page/LandingPage.js
@@ -1,22 +1,21 @@
 import React, {useEffect,useState} from 'react';
 import { withRouter, Link } from 'react-router-dom';
-import LandingHeader from '../components/Header/LandingHeader';
 import AllPosts from '../view/Post/AllPosts';
 import axios from 'axios'
 
 function LandingPage(props){
 
+    const [state, setState] = useState("")
     // 로그인 정보 가져오기
     useEffect(() => {
-        axios.get('/api/users/profile').then((req, res) =>setState(req.data))
-       
+        axios.get('/api/users/profile').then((req) =>setState(req.data))
+        
+        
     }, [])
     
-    const [state, setState] = useState("")
+    
     console.log(state)
     const id = state.id // url에 parameter를 전달하기 위해서 id 변수생성
-    const name = state.name
-    console.log(id)
 
     return(
         <div>
