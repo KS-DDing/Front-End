f6c1a424c8922dc8b2b0467fc24179b023db2aec
DDing77 <smg76262585@gmail.com>
2021-08-29 21:08:46 +0900

    Update MyPosts.js

--- src/view/Post/MyPosts.js
+++ src/view/Post/MyPosts.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import {Link} from 'react-router-dom'
 import axios from 'axios';
 import '@toast-ui/editor/dist/toastui-editor-viewer.css';
 import { Viewer } from '@toast-ui/react-editor';
@@ -11,15 +12,15 @@ export default function MyPosts(props) {
 
     const id = props.id.id
 
-    useEffect( () => {
+    useEffect(() => {
         const GetMyPosts = async () => {
-            try{
+            try {
                 setError(null);
                 setData('');
                 setLoading(true);
                 const response = await axios.get(`/api/posts/${id}`)
                 setData(response.data[0].posts);
-            } catch(e) {
+            } catch (e) {
                 setError(e)
             }
             setLoading(false);
@@ -27,47 +28,52 @@ export default function MyPosts(props) {
         GetMyPosts();
     }, [])
 
-    if(loading) return <div>로딩중...</div>
-    if(error) return <div>에러가 발생했습니다.</div>
-    if(!data) return null;
+    if (loading) return <div>로딩중...</div>
+    if (error) return <div>에러가 발생했습니다.</div>
+    if (!data) return null;
 
-    console.log(data[0].posts)
-
+    console.log(data)
     const covertDate = (createAt) => {
         let newDate = Date(createAt)
 
-        
+
         return newDate
     }
 
-    const rederAllPosts = () => (
-        
+    const onClickButtonListener = (event) => {
+        const requesetData = data
+
+    }
+
+    const rederMyPosts = () => (
+
         data.map((content, index) => (
             // console.log(index)
             // console.log(content.id)
-            
-            <div>
-                <ul key={index}>
-                    <li>썸네일: {content.images}</li>
-                    <li>내용: <Viewer
-                                viewer="true"
-                                initialEditType="markdown"
-                                initialValue={content.content}
-                                />
-                    </li>
-                    <li>해쉬태그: {content.hashtags}</li>
-                    <li>작성자: {content.userId}</li>
-                    <li>작성날짜: {covertDate(content.createAt)}</li>
-                    <li>추천수: {content.liker}</li>
-                </ul>
+            <div style={{ marign: "auto" }}>
+                <Link to={`/viewer/${index+1}`}><button onClick={"onClickButtonListener"}>
+                    <ul key={index}>
+                        <li>썸네일: {content.images}</li>
+                        <li>내용: <Viewer
+                            viewer="true"
+                            initialEditType="markdown"
+                            initialValue={content.content}
+                        />
+                        </li>
+                        <li>해쉬태그: {content.hashtags}</li>
+                        <li>작성자: {content.userId}</li>
+                        <li>작성날짜: {covertDate(content.createAt)}</li>
+                        <li>추천수: {content.liker}</li>
+                    </ul>
+                </button></Link>
             </div>
         ))
     )
 
     return (
         <div>
-        {rederAllPosts()}
-        {/* <Viewer
+            {rederMyPosts()}
+            {/* <Viewer
             viewer="true"
             initialEditType="markdown"
             initialValue={data.content}
