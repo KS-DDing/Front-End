05b75d9651c5050e855cf0263bd465ac06ce9264
DDing77 <smg76262585@gmail.com>
2021-08-31 06:19:52 +0900

    Update Personal.js

--- src/Page/Personal.js
+++ src/Page/Personal.js
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import '@toast-ui/editor/dist/toastui-editor-viewer.css';
 import { Viewer } from '@toast-ui/react-editor';
 import MyPost from '../view/Post/MyPosts'
-function PersonalPage({match}) {
+function PersonalPage({ match }) {
     console.log(match.params)
-    return(
+    return (
         <div>
             <h1>여긴 개인화면^^7 자기글만 보임</h1>
-            <MyPost id={match.params}/>
+            <Link to={{
+                pathname: "/post",
+                state: match.params
+            }}>글 새로 작성하기</Link>
+            <MyPost id={match.params} />
         </div>
-        
-        
+
+
     )
 }
 
